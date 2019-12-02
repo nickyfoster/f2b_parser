47f40736e51c1c6d1f9470bdb2f47b1c2e1b81ed
nickyfoster <nik9516@gmail.com>
2019-12-02 23:50:34 +0300

    redis added

--- F2BParser/F2BParser.py
+++ F2BParser/F2BParser.py
@@ -1,6 +1,7 @@
 import time
 import datetime
 from utils.utils import get_redis, get_option_from_config
+import json
 
 
 class F2BParser:
@@ -8,49 +9,42 @@ class F2BParser:
         self.redis = get_redis()
         self.f2b_logfile_path = get_option_from_config(
             ["F2B_logfile_path"]) if f2b_logfile_path is None else f2b_logfile_path
-        self.ips_file_path = get_option_from_config(["IPs_logfile_path"]) if ips_file_path is None else ips_file_path
-        print(self.f2b_logfile_path, 1)
         self.main_loop()
 
     def main_loop(self):
         while True:
             self.__update_ips_logfile()
-            time.sleep(3)
-
-    def test(self):
-        pass
+            time.sleep(1)
 
     def __update_ips_logfile(self):
-        current_ips = self.__get_ips_log()
         banned_ips = self.__get_banned_ips_from_f2b_log()
         unbanned_ips = self.__get_unbanned_ips_from_f2b_log()
-
         for ip in banned_ips:
-            if ip not in current_ips:
+            if not self.redis.exists(ip):
                 try:
                     if unbanned_ips[ip] < banned_ips[ip]:
-                        self.__add_ip(ip=ip)
+                        self.__add_ip(ip=ip, ts=banned_ips[ip])
                 except KeyError:
-                    self.__add_ip(ip=ip)
+                    self.__add_ip(ip=ip, ts=banned_ips[ip])
         for ip in unbanned_ips:
-            if ip in current_ips:
+            if self.redis.exists(ip):
                 try:
                     if unbanned_ips[ip] > banned_ips[ip]:
                         self.__delete_ip(ip=ip)
                 except KeyError:
                     self.__delete_ip(ip=ip)
 
+    def __add_ip(self, ip, ts):
+        print(f"Adding {ip}")
+        self.redis.set(ip, ts)
+
+    def __delete_ip(self, ip):
+        print(f"Deleting {ip}")
+        self.redis.delete(ip)
+
     def __get_f2b_log(self):
         with open(self.f2b_logfile_path, 'r') as f:
             lines = f.readlines()
-        return lines
-
-    def __get_ips_log(self):
-        try:
-            with open(self.ips_file_path, 'r') as f:
-                lines = f.read().splitlines()
-        except FileNotFoundError:
-            return []
         return lines
 
     def __get_banned_ips_from_f2b_log(self):
@@ -72,19 +66,6 @@ class F2BParser:
                 ip_timestamp = time.mktime(datetime.datetime.strptime(ip_timestamp, "%Y-%m-%d %H:%M:%S,%f").timetuple())
                 unbanned_ips_list[line.split()[-1]] = ip_timestamp
         return unbanned_ips_list
-
-    def __delete_ip(self, ip: str):
-        print(f"Removing {ip} from list")
-        lines = self.__get_ips_log()
-        with open(self.ips_file_path, "w") as f:
-            for line in lines:
-                if line.strip("\n") != ip:
-                    f.write(line + '\n')
-
-    def __add_ip(self, ip: str):
-        print(f"Adding {ip} to list")
-        with open(self.ips_file_path, 'a') as f:
-            f.write(ip + '\n')
 
 
 parser = F2BParser()
