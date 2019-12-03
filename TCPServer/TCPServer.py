334807cf2090570c5081431eab004617c21c9123
nickyfoster <nik9516@gmail.com>
2019-12-04 00:21:21 +0300

    added TCPServer

---
+++ TCPServer/TCPServer.py
@@ -0,0 +1,28 @@
+import socket
+
+
+class TCPServer:
+    BUFFER_SIZE = 1024
+
+    def __init__(self, host='', port=7070):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket()
+        self.running = True
+        self.data = None
+
+    def run_server(self):
+        print(f"Starting TCP server on port {self.port}")
+        self.sock.bind((self.host, self.port))
+        sock.listen(10)
+        conn, addr = self.sock.accept()
+        print('connected:', addr)
+
+        self.listen_socket(conn, addr)
+
+    def listen_socket(self, conn, addr):
+        while self.running:
+            self.data = conn.recv(BUFFER_SIZE)
+            print(data)
+        self.conn.close()
+        print("Server stopped")
